f311f001448c2e5a500102386271f22de892c3fe
RockoGilbert <evolve2122@gmail.com>
2022-08-04 16:17:51 -0500

    Edited Files

--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -1,14 +1,11 @@
+
 #ifndef SANDPILES_H
 #define SANDPILES_H
 
-#include <stdlib.h>
-#include <stdio.h>
-
+void grid_addition(int grid1[3][3], int grid2[3][3]);
+void grid_print(int grid[3][3]);
+int grid_is_ok(int grid[3][3]);
+void grid_change(int grid1[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
 
-/** Auxiliar functions */
-void sum_grids(int grid1[3][3], int grid2[3][3]);
-int is_sandpile(int grid1[3][3]);
-void convert_sandpile(int grid1[3][3]);
-
-#endif /** SANDPILES_H */
+#endif
