627aee084669e34f770d8467f52a615ba231c8c5
Rocko Gilbert <evolve2122@gmail.com>
2022-11-30 13:51:14 -0600

    Update 0-island_perimeter.py

--- 0x15-island_perimeter/0-island_perimeter.py
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -12,6 +12,8 @@ def island_perimeter(grid):
                 perimeter += 4
                 # check if the cell is connected to the NESW neighbors
                 # if it is, subtract 2 from the perimeter
+                
+                
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
